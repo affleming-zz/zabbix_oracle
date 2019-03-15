967aa5c17dc5063c2c5e81b3c649ba8c355affec
André Felipe Fleming <andref.fleming@gmail.com>
2019-03-15 16:29:20 -0300

    tb_pdb.sql

--- tb_pdb.sql
+++ tb_pdb.sql
@@ -3,7 +3,7 @@ SET      heading OFF
 SET      feedback OFF
 SET	 verify OFF
 SELECT 
-' Tablespace: ' || lower(m.tablespace_name) || ' | Utilizado: ' || round(m.used_percent) || '%' || ' | PDB: ' || lower(p.pdb_name) || ' || '
+' Tablespace: ' || lower(m.tablespace_name) || ' | Utilizado: ' || round(m.used_percent) || '%' || ' | PDB: ' || lower(p.pdb_name)
 FROM cdb_tablespace_usage_metrics m INNER JOIN cdb_pdbs p
 ON m.con_id = p.con_id
 WHERE round(m.used_percent) >= 90
